da1119a06d3a44539405d5381f171dba039593d5
Anuradha Dilshan <101551005+AnuradhaDilshan@users.noreply.github.com>
2024-03-07 19:05:04 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -16,7 +16,6 @@ const Footer = () => {
           <div className="row">
             <div className="col-md-6 col-lg-5 col-12 ft-1">
               {/* <img src={footer_logo} alt="" /> */}
-              <p>Government Approved Grade "B" Learners</p>
               <div className="footer-icons">
                 <a
                   href="https://www.facebook.com/profile.php?id=100068924735662"
@@ -49,61 +48,6 @@ const Footer = () => {
                   <i class="fa-brands fa-linkedin-in"></i>
                 </a>
               </div>
-            </div>
-            <div className="col-md-6 col-lg-3 col-12 ft-2">
-              <h5>Quick Links</h5>
-              <ul>
-                <li className="nav-item">
-                  <NavLink to="/services" onClick={scrollToTop}>
-                    Services
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/journey" onClick={scrollToTop}>
-                    Journey
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/gallery" onClick={scrollToTop}>
-                    Gallery
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/about" onClick={scrollToTop}>
-                    About Us
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/instructors" onClick={scrollToTop}>
-                    Instructors
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-6 col-lg-4 col-12 ft-3">
-              <h5 className="contact">Contact Us</h5>
-              <p>
-                <i class="fa-solid fa-location-dot"></i>
-                {"City Bypass Road, Elpitiya"}
-              </p>
-              <p>
-                <i class="fa-solid fa-phone"></i>
-                {"0915010025"}
-                <br />
-                <p className="phone">
-                  {"0729331679"}
-                  <br />
-                  {"0778719826"}
-                </p>
-              </p>
-              <p className="mail">
-                <i class="fa-solid fa-envelope"></i>
-                {"savingolearners@gmail.com"}
-              </p>
-              <p>
-                <i class="fa-solid fa-check"></i>
-                {"Reg. No: D/S 828"}
-              </p>
             </div>
           </div>
         </div>
